d4fb75d9b4629415dc8fdc25f6d1a4f826e74491
Nishant Kumar <132368528+nishantkm-2308@users.noreply.github.com>
2024-10-19 17:27:10 +0530

    Add files via upload

---
+++ number_conversion.py
@@ -0,0 +1,9 @@
+#NUMBER CONVERSION IN PYTHON =>
+
+n = 25
+
+print(bin(n))  #decimal to binary (output is 0b11001 but binary of 25 is "11001" and 0b shows that it's in binary format) 
+
+print(oct(n))  #decimal to octal 
+
+print(hex(n))  #decimal to hexadecimal
